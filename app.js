da856332773e336638863327add24662eec989d9
Jeff Wong <jeffmjwong@gmail.com>
2018-03-05 09:03:40 +1000

    Query selector

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const myHeading = document.getElementsByTagName('h1')[0];
 const myButton1 = document.getElementById('myButton1');
 const myButton2 = document.getElementById('myButton2');
+const myButton3 = document.getElementById('myButton3');
 const myTextInput = document.getElementById('myTextInput');
 const myList = document.getElementsByTagName('li');
-const errorNotPurple = document.getElementsByClassName('error-not-purple');
+const errorNotPurple = document.querySelectorAll('.error-not-purple');
+const evens = document.querySelectorAll('li:nth-child(even)');
 
 
 // myButton.addEventListener('click', () => {
@@ -20,5 +22,11 @@ myButton1.addEventListener('click', () => {
 myButton2.addEventListener('click', () => {
   Array.from(errorNotPurple).forEach(element => {
     element.style.color = "orange";
+  });
+});
+
+myButton3.addEventListener('click', () => {
+  Array.from(evens).forEach(element => {
+    element.style.color = "blue";
   });
 });
