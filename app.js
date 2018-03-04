f2ebf1e3cf6984311db58ef4c0d529d44cb73bc2
Jeff Wong <jeffmjwong@gmail.com>
2018-03-04 20:25:48 +1000

    Select html elements by id, tag names and class names

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-const myHeading = document.getElementById('myHeading');
-const myButton = document.getElementById('myButton');
-const myTextInput = document.getElementById('myTextInput')
+const myHeading = document.getElementsByTagName('h1')[0];
+const myButton1 = document.getElementById('myButton1');
+const myButton2 = document.getElementById('myButton2');
+const myTextInput = document.getElementById('myTextInput');
+const myList = document.getElementsByTagName('li');
+const errorNotPurple = document.getElementsByClassName('error-not-purple');
 
 
-myButton.addEventListener('click', () => {
-  const inputValue = myTextInput.value;
-  myHeading.style.color = inputValue;
+// myButton.addEventListener('click', () => {
+//   const inputValue = myTextInput.value;
+//   myHeading.style.color = inputValue;
+// });
+
+myButton1.addEventListener('click', () => {
+  Array.from(myList).forEach(element => {
+    element.style.color = "red";
+  });
+});
+
+myButton2.addEventListener('click', () => {
+  Array.from(errorNotPurple).forEach(element => {
+    element.style.color = "orange";
+  });
 });
