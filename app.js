be0429e1adf2a9703e204adef9a97dd7be4f40e7
Jeff Wong <jeffmjwong@gmail.com>
2018-03-06 09:50:27 +1000

    Change heading color

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const myHeading = document.getElementsByTagName('h1')[0];
+const myButton0 = document.getElementById('myButton0');
 const myButton1 = document.getElementById('myButton1');
 const myButton2 = document.getElementById('myButton2');
 const myButton3 = document.getElementById('myButton3');
 const myTextInput = document.getElementById('myTextInput');
+const myTextInput0 = document.getElementById('myTextInput0');
 const myList = document.getElementsByTagName('li');
 const errorNotPurple = document.querySelectorAll('.error-not-purple');
 const evens = document.querySelectorAll('li:nth-child(even)');
 
 
-// myButton.addEventListener('click', () => {
-//   const inputValue = myTextInput.value;
-//   myHeading.style.color = inputValue;
-// });
+myButton0.addEventListener('click', () => {
+  const inputValue = myTextInput0.value;
+  myHeading.style.color = inputValue;
+});
 
 myButton1.addEventListener('click', () => {
   Array.from(myList).forEach(element => {
